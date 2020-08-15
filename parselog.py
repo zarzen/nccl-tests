aff11099055b07b0d8f2e0170b53340b1ce8b075
Zhen Zhang <zhangzhen.dev@gmail.com>
2020-08-15 16:05:45 -0400

    update parser

--- parselog.py
+++ parselog.py
@@ -1,10 +1,11 @@
 from collections import defaultdict
-
+import sys
 
 def main():
     """"""
     size_count = defaultdict(int)
-    with open("./run.log") as ifile:
+    filename = sys.argv[1]
+    with open(filename) as ifile:
         for line in ifile:
             if "socketProgressOpt::" in line:
                 splits = line.split(':')
@@ -13,9 +14,12 @@ def main():
     keys = list(size_count.keys())
     total_times = sum(size_count.values())
     keys.sort()
+    total_size = 0
     for k in keys:
         s = "{}, {}, {:.3f}%".format(k, size_count[k], (size_count[k]/total_times)*100)
         print(s)
+        total_size += (k * size_count[k])
+    print('transfer send/recv', total_size / 1e6, "MB")
 
 
 if __name__ == "__main__":
